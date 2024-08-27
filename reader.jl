76a42ac81d0dc86c30423c9ec6d1ff4844584930
Neel Smith <dnsmith.neel@gmail.com>
2024-08-27 08:03:33 -0400

    Fix bug in checking for online status

--- reader.jl
+++ reader.jl
@@ -18,6 +18,8 @@ function blackwell_sort(matches, lemmastring)
 	vcat(top, middle, bottom)
 end
 
+"""Format a list of data lines from delimited-text source as a single Markdown text.
+"""
 function format(entries)
     formatted = map(entries) do entry
         cols = split(entry,"|")
@@ -29,7 +31,9 @@ function format(entries)
     join(formatted, "\n\n")
 end
 
-function read_ls(remote = true)
+
+"""Read Lewis-Short articles."""
+function read_ls(f = "ls-articles.ex"; remote = true)
     if remote
         url = "http://shot.holycross.edu/lexica/ls-articles.cex"
         f = Downloads.download(url)
@@ -38,12 +42,11 @@ function read_ls(remote = true)
         content
 
     else
-        f = "ls-articles.cex"
         readlines(f)
     end
 end
 
-
+"""Search a list of articles for text matching a string."""
 function text(s; articles = articles)
     matches = filter(article -> occursin(s,article), articles)
     formatted = blackwell_sort(matches, s) |> format
@@ -52,6 +55,7 @@ function text(s; articles = articles)
     string(hdr, formatted)
 end
 
+"""Search a list of articles for identifying ID matching a string."""
 function id(s; articles = articles)
     pttrn = "urn:cite2:hmt:ls.markdown:" * s * "\\|"
     re = Regex(pttrn)
@@ -62,8 +66,10 @@ function id(s; articles = articles)
     string(hdr, formatted)
 end
 
+"""Search a list of articles for lemma matching a string,
+optionally limiting search to matching the beginning of the lemma.
+"""
 function lemma(s; articles = articles, initial = false)
-    #pttrn =  initial ? "\\|$(s).*\\|.+" : "$(s).*\\|.+"
     pttrn =  initial ? "[^\\|]+\\|[^\\|]+\\|$(s).*\\|.+" : "[^\\|]+\\|[^\\|]+\\|[^\\|]*$(s).*\\|.+"
     re = Regex(pttrn)
     matches = filter(article -> occursin(re,article), articles)
@@ -85,10 +91,16 @@ end
 
 @info("Script version: $(scriptversion)")
 @info("To see version info:")
-@info("   versioninfo() |> Markdown.parse\n\n")
+@info("   versioninfo() |> Markdown.parse\n")
 @info("Downloading Lewis-Short dictionary...")
-articles = read_ls()
-@info("Complete.")
+try
+    global articles = read_ls()
+    @info("Complete.")
+catch
+    @warn("\nCouldn't download dictionary data.")
+    @info("\nIf you have a local copy, you can use it by running:")
+    @info("    articles = read_ls(remote = false)\n")
+end
 @info("\nUse one of these to view formatted articles in your REPL.")
 @info("\nFind article by ID:")
 @info("    id(IDVALUE) |> Markdown.parse\n")
